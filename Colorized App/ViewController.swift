0445e18f92464b7baaaccf7f4ca17c7a0b3ef824
Igor Sokolov <igor0401@icloud.com>
2022-06-24 13:35:58 +0300

    Setting up sliders

--- Colorized App/ViewController.swift
+++ Colorized App/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpRedSlider()
+        setUpGreenSlider()
+        setUpBlueSlider()
     }
-
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        
+    }
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+        
+    }
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setUpRedSlider() {
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+    }
+    
+    private func setUpGreenSlider() {
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+    }
+    
+    private func setUpBlueSlider() {
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+    }
 
 }
 
